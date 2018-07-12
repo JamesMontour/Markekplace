454dd1eed13f598d5c903f4c30c5849cb31c52ae
James Montour <JamesMontour@users.noreply.github.com>
2018-07-12 18:13:35 +0000

    Delete giverole.js

--- commands/giverole.js
+++
@@ -1,7 +0,0 @@
-exports.run = (client, msg, args) => {
-    if (msg.channel.permissionsFor(msg.author).has("KICK_MEMBERS")) {
-        msg.channel.send('Coming Soon~~~');
-    } else {
-        msg.channel.send('You do not have permission to use this command!');
-    }
-}
