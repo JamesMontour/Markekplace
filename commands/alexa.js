df20581433bb2638dfe38b31dfeaafb9f28c95c2
James Montour <JamesMontour@users.noreply.github.com>
2018-08-03 07:50:58 -0400

    Update alexa.js

--- commands/alexa.js
+++ commands/alexa.js
@@ -1,13 +1,12 @@
 exports.run = (client, msg, args) => {
     let command = args[0];
     let request = args[1];
-    requestStr = request.toString();
-    requestLow = requestStr.toLowerCase();
+    request = request.toString().toLowerCase();
 
     if (command === "play") {
-        if (requestLow === "despacito") {
+        if (request === "despacito") {
             msg.channel.send("Despacito, Quiero respirar tu cuello despacito, Deja que te diga cosas al oído, Para que te acuerdes si no estás conmigo, Despacito", {tts: true}).then(console.log).catch(console.error);
-        } else if (requestLow != null && requestLow != "despacito") {
+        } else if (request != null && request != "despacito") {
             msg.channel.send("fuck off, cunt").then(console.log).catch(console.error);
         } else {
             msg.channel.send("Enter a valid song request.").then(console.log).catch(console.error);
